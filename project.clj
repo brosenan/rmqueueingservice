113f0dd64d8c8daaa7bb8fc00a5d32f47941fc25
Boaz Rosenan <brosenan@gmail.com>
2018-09-05 15:59:21 +0300

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brosenan/rmqueueingservice "0.1.5-SNAPSHOT"
+(defproject brosenan/rmqueueingservice "0.1.5"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
